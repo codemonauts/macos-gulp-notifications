b6c5921d9c40f71293f7c80cba1e1402b0f1bb24
Thomas Schulze <tschulze@codemonauts.com>
2017-10-07 23:36:39 +0200

    Initial Commit

---
+++ GulpNotifications/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GulpNotifications
+//
+//  Created by Thomas Schulze on 07.10.17.
+//  Copyright © 2017 codemonauts UG (haftungsbeschränkt). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
